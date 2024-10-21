430be6825f056ef7e00d06266367ec34e97ccebb
Dablestar <jbynchoi@gmail.com>
2024-10-22 10:38:16 +1100

    divided border color changer and UI manager, added Manager object

---
+++ Assets/Scripts/BorderColorChanger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BorderColorChanger : MonoBehaviour
+    {
+        private Image borderLine;
+        private float destination;
+        private Color[] colorList;
+        private int startIndex;
+        private int endIndex;
+        private float time;
+
+        private void Start()
+        {
+            startIndex = 0;
+            endIndex = 1;
+            colorList = new Color[]
+            {
+                Color.white,
+                Color.blue,
+                Color.red,
+                Color.black
+            };
+            time = 1.5f;
+            borderLine = GameObject.Find("Border").GetComponent<Image>();
+        }
+
+        private void Update()
+        {
+            throw new NotImplementedException();
+        }
+        
+        void BorderColorChange()
+        {
+            destination += Time.deltaTime / time;
+            borderLine.color = Color.Lerp(colorList[startIndex], colorList[endIndex], destination);
+            if (destination >= 1f)
+            {
+                destination = 0f;
+                startIndex = endIndex;
+                endIndex = endIndex >= 3 ? 0 : endIndex += 1;
+            }
+        }
+    }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,58 +9,39 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    private Image borderLine;
-    private float destination;
-    private Color[] colorList;
-    private int startIndex;
-    private int endIndex;
-    private float time;
+    private int score;
+    private Time time;
     
     // Start is called before the first frame update
     void Start()
     {
-        startIndex = 0;
-        endIndex = 1;
-        colorList = new Color[]
-        {
-            Color.white,
-            Color.blue,
-            Color.red,
-            Color.black
-        };
-        time = 1.5f;
-        borderLine = GameObject.Find("Border").GetComponent<Image>();
-        Debug.Log(borderLine.color == Color.white);
+        time = new Time();
     }
 
     // Update is called once per frame
     void Update()
     {
-        BorderColorChange();
+        
     }
     
     public void MoveToMainScene()
     {
+        Debug.Log("clicked");
+        DontDestroyOnLoad(this);
         SceneManager.LoadScene("MainScene");
     }
 
     public void MoveToDesignIteration()
     {
+        DontDestroyOnLoad(this);
         Debug.Log("Not Implemented");
     }
 
-    void BorderColorChange()
+    public void MoveToStartScene()
     {
-        destination += Time.deltaTime / time;
-        borderLine.color = Color.Lerp(colorList[startIndex], colorList[endIndex], destination);
-        Debug.Log(borderLine.color + " ||" + colorList[endIndex] + " DEST : " + destination );
-        Debug.Log(startIndex + ", " + endIndex);
-        if (destination >= 1f)
-        {
-            destination = 0f;
-            startIndex = endIndex;
-            endIndex = endIndex >= 3 ? 0 : endIndex += 1;
-        }
+        
     }
+
+    
     
 }
